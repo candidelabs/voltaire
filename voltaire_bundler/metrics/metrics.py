c2256278e13b2129e732145ced52c66dfabdff22
Sherif Abdelmoatty <sherif.ahmed990@gmail.com>
2023-07-02 04:02:32 +0300

    remove metrics

--- voltaire_bundler/metrics/metrics.py
+++
@@ -1,6 +0,0 @@
-import logging
-from prometheus_client import start_http_server
-
-def run_metrics_server(port=8000):
-    logging.info(f"Starting Metrics Server at: {port}")
-    start_http_server(port)
