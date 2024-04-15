e387667412f349c102adfa4b25cc2846fc09e5ac
Sherif Abdelmoatty <sherif.ahmed990@gmail.com>
2024-04-15 19:20:26 +0300

    Add "Arbitrum Sepolia" mempool id

--- voltaire_bundler/bundler/mempool/mempool_info.py
+++ voltaire_bundler/bundler/mempool/mempool_info.py
@@ -4,5 +4,6 @@ DEFAULT_MEMPOOL_INFO = {
         5         : "QmTmj4cizhWpEFCCqk5dP67yws7R2PPgCtb2bd2RgVPCbF",#goerli
         80001     : "QmcFZKUX9qoo3StwVycJTAhwdiqbEjcwNvQVK246p3z1rk",#mumbai
         11155111  : "QmdDwVFoEEcgv5qnaTB8ncnXGMnqrhnA5nYpRr4ouWe4AT",#sepolia
+        421614  : "QmVwhF77aVNzRUkMJNLDkeF9BtQMHLnfDY5ePpZ81uKLzA",#arbitrum sepolia
     }
 }
