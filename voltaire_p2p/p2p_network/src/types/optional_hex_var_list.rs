b2f658e14f75f25a6e8a7eec5352934cd6fef3c6
Sherif Abdelmoatty <sherif.ahmed990@gmail.com>
2024-10-20 12:06:55 +0300

    fix optional_hex_var_list to not revert if None

--- voltaire_p2p/p2p_network/src/types/optional_hex_var_list.rs
+++ voltaire_p2p/p2p_network/src/types/optional_hex_var_list.rs
@@ -7,10 +7,10 @@ pub fn serialize<S, N>(bytes_optional: &Option<VariableList<u8, N>>, serializer:
 where
     S: Serializer,
     N: Unsigned,
-{
+{   
     match bytes_optional{
         Some(bytes_optional)=> serializer.serialize_str(&hex::encode(&**bytes_optional)),
-        _=>serializer.serialize_str("0x")
+        None=> serializer.serialize_str("0x")
     }
 }
 
@@ -18,8 +18,8 @@ pub fn deserialize<'de, D, N>(deserializer: D) -> Result<Option<VariableList<u8,
 where
     D: Deserializer<'de>,
     N: Unsigned,
-{
-    let bytes = deserializer.deserialize_str(PrefixedHexVisitor)?;
+{ 
+    let bytes = deserializer.deserialize_str(PrefixedHexVisitor).unwrap_or(vec![]);
 
     match VariableList::new(bytes){
         Ok(number)  => Ok(Some(number)),
