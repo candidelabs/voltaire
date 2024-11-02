0cd5416a8464415fb226cd6b1d2463ed776bcb4a
Sherif Abdelmoatty <sherif.ahmed990@gmail.com>
2024-11-02 13:50:19 +0300

    add dummy v0.07 mempool id's

--- voltaire_bundler/mempool/mempool_info.py
+++ voltaire_bundler/mempool/mempool_info.py
@@ -6,11 +6,12 @@ DEFAULT_MEMPOOL_INFO = {
         11155111: "QmdDwVFoEEcgv5qnaTB8ncnXGMnqrhnA5nYpRr4ouWe4AT",  # sepolia
         421614: "QmVwhF77aVNzRUkMJNLDkeF9BtQMHLnfDY5ePpZ81uKLzA",  # arbitrum sepolia
     },
+    #dummy v0.07 mempool id's for now
     "0x0000000071727De22E5E9d8BAf0edAc6f37da032": {
-        1337: "Qmf7P3CuhzSbpJa8LqXPwRzfPqsvoQ6RG7aXvthYTzGxb2",  # dev
-        5: "QmTmj4cizhWpEFCCqk5dP67yws7R2PPgCtb2bd2RgVPCbF",  # goerli
-        80001: "QmcFZKUX9qoo3StwVycJTAhwdiqbEjcwNvQVK246p3z1rk",  # mumbai
-        11155111: "QmdDwVFoEEcgv5qnaTB8ncnXGMnqrhnA5nYpRr4ouWe4AT",  # sepolia
-        421614: "QmVwhF77aVNzRUkMJNLDkeF9BtQMHLnfDY5ePpZ81uKLzA",  # arbitrum sepolia
+        1337: "Qmf7P3CuhzSbpJa8LqXPwRzfPqsvoQ6RG7aXvthYTzGxb1",  # dev
+        5: "QmTmj4cizhWpEFCCqk5dP67yws7R2PPgCtb2bd2RgVPCb1",  # goerli
+        80001: "QmcFZKUX9qoo3StwVycJTAhwdiqbEjcwNvQVK246p3z1r1",  # mumbai
+        11155111: "QmdDwVFoEEcgv5qnaTB8ncnXGMnqrhnA5nYpRr4ouWe4A1",  # sepolia
+        421614: "QmVwhF77aVNzRUkMJNLDkeF9BtQMHLnfDY5ePpZ81uKLz1",  # arbitrum sepolia
     }
 }
