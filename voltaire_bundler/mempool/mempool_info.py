4d7bbada0df78bb5c6d4ef8be71138ed02916929
Sherif Abdelmoatty <sherif.ahmed990@gmail.com>
2024-11-08 19:09:25 +0300

    add ethereum, optimism and arbitrum mempool ids
    (cherry picked from commit 07104ce601876b46606e259f2ef65b6f79ec152e)

--- voltaire_bundler/mempool/mempool_info.py
+++ voltaire_bundler/mempool/mempool_info.py
@@ -1,7 +1,10 @@
 DEFAULT_MEMPOOL_INFO = {
     "0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789": {
         1337: "Qmf7P3CuhzSbpJa8LqXPwRzfPqsvoQ6RG7aXvthYTzGxb2",  # dev
+        1: "QmVEt8BqyX7mbPhMNkmhnxL7fLxcXxsReMQcjYMBSHBfy7",  # ethereum
         5: "QmTmj4cizhWpEFCCqk5dP67yws7R2PPgCtb2bd2RgVPCbF",  # goerli
+        10: "QmPkygym9oarrdiTeGBFQqbJcjpv4yHLLXrqQYGqKiXs7s",  # optimism
+        42161: "QmSpr2Q6cMfZ2CvXecH843KtvnG3tzvxZVy1jKphYKd6tf",  # arbitrum
         80001: "QmcFZKUX9qoo3StwVycJTAhwdiqbEjcwNvQVK246p3z1rk",  # mumbai
         11155111: "QmdDwVFoEEcgv5qnaTB8ncnXGMnqrhnA5nYpRr4ouWe4AT",  # sepolia
         421614: "QmVwhF77aVNzRUkMJNLDkeF9BtQMHLnfDY5ePpZ81uKLzA",  # arbitrum sepolia
@@ -9,7 +12,10 @@ DEFAULT_MEMPOOL_INFO = {
     #dummy v0.07 mempool id's for now
     "0x0000000071727De22E5E9d8BAf0edAc6f37da032": {
         1337: "Qmf7P3CuhzSbpJa8LqXPwRzfPqsvoQ6RG7aXvthYTzGxb1",  # dev
+        1: "QmVEt8BqyX7mbPhMNkmhnxL7fLxcXxsReMQcjYMBSHBfy1",  # ethereum
         5: "QmTmj4cizhWpEFCCqk5dP67yws7R2PPgCtb2bd2RgVPCb1",  # goerli
+        10: "QmPkygym9oarrdiTeGBFQqbJcjpv4yHLLXrqQYGqKiXs71",  # optimism
+        42161: "QmSpr2Q6cMfZ2CvXecH843KtvnG3tzvxZVy1jKphYKd6t1",  # arbitrum
         80001: "QmcFZKUX9qoo3StwVycJTAhwdiqbEjcwNvQVK246p3z1r1",  # mumbai
         11155111: "QmdDwVFoEEcgv5qnaTB8ncnXGMnqrhnA5nYpRr4ouWe4A1",  # sepolia
         421614: "QmVwhF77aVNzRUkMJNLDkeF9BtQMHLnfDY5ePpZ81uKLz1",  # arbitrum sepolia
