69398cfb5d755e0c44634233072f8e4e27c0aa37
Sherif Abdelmoatty <sherif.ahmed990@gmail.com>
2025-03-27 09:22:50 +0200

    Bump version

--- voltaire_p2p/p2p_network/voltaire_version/src/lib.rs
+++ voltaire_p2p/p2p_network/voltaire_version/src/lib.rs
@@ -1,12 +1,12 @@
 use target_info::Target;
 
-pub const VERSION: &str = "Voltaire/v0.1.0a42";
+pub const VERSION: &str = "Voltaire/v0.1.0a43";
 
 /// Returns `VERSION`, but with platform information appended to the end.
 ///
 /// ## Example
 ///
-/// `Voltaire/v0.1.0a42/x86_64-linux`
+/// `Voltaire/v0.1.0a43/x86_64-linux`
 pub fn version_with_platform() -> String {
     format!("{}/{}-{}", VERSION, Target::arch(), Target::os())
 }
