5bccba0cbddb3b5a73fb3dedcb8cc8e73addbee9
Sherif Abdelmoatty <sherif.ahmed990@gmail.com>
2025-06-09 12:57:45 +0300

    update DEFAULT_MEMPOOL_INFO

--- voltaire_bundler/mempool/mempool_info.py
+++ voltaire_bundler/mempool/mempool_info.py
@@ -2,30 +2,24 @@ DEFAULT_MEMPOOL_INFO = {
     "0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789": {
         1337: "Qmf7P3CuhzSbpJa8LqXPwRzfPqsvoQ6RG7aXvthYTzGxb2",  # dev
         1: "QmVEt8BqyX7mbPhMNkmhnxL7fLxcXxsReMQcjYMBSHBfy7",  # ethereum
-        5: "QmTmj4cizhWpEFCCqk5dP67yws7R2PPgCtb2bd2RgVPCbF",  # goerli
         10: "QmPkygym9oarrdiTeGBFQqbJcjpv4yHLLXrqQYGqKiXs7s",  # optimism
         42161: "QmSpr2Q6cMfZ2CvXecH843KtvnG3tzvxZVy1jKphYKd6tf",  # arbitrum
-        80001: "QmcFZKUX9qoo3StwVycJTAhwdiqbEjcwNvQVK246p3z1rk",  # mumbai
         11155111: "QmdDwVFoEEcgv5qnaTB8ncnXGMnqrhnA5nYpRr4ouWe4AT",  # sepolia
         421614: "QmVwhF77aVNzRUkMJNLDkeF9BtQMHLnfDY5ePpZ81uKLzA",  # arbitrum sepolia
     },
     "0x0000000071727De22E5E9d8BAf0edAc6f37da032": {
         1337: "QmcBcc4nxdGiKyM6RC7fZAUtsYNaAYqfzf9wfNxYmdT9Fc",  # dev
         1: "Qma9Fv7qSahePTd5ACCFG8Jsd3qJqtFUqGmmGKoVqriajp",  # ethereum
-        5: "QmTmj4cizhWpEFCCqk5dP67yws7R2PPgCtb2bd2RgVPCb1",  # goerli
         10: "QmcBcc4nxdGiKyM6RC7fZAUtsYNaAYqfzf9wfNxYmdT9Fc",  # optimism
         42161: "QmXH8oBNx1EjxYkSWpk8DMcyJgMxCk8Dujz1PswQGdGFyK",  # arbitrum
-        80001: "QmcFZKUX9qoo3StwVycJTAhwdiqbEjcwNvQVK246p3z1r1",  # mumbai
-        11155111: "QmdDwVFoEEcgv5qnaTB8ncnXGMnqrhnA5nYpRr4ouWe4A1",  # sepolia
-        421614: "QmVwhF77aVNzRUkMJNLDkeF9BtQMHLnfDY5ePpZ81uKLz1",  # arbitrum sepolia
+        11155111: "QmcpcBuF5qiHJN1znC1x6c43iN54SR4nawm9nUpcRnk8RQ",  # sepolia
+        421614: "Qmc3g1MCwfrkaTrJ6e1bWTwdiLiz1muzKcqrYgzcSJJXaK",  # arbitrum sepolia
     },
     "0x4337084d9e255ff0702461cf8895ce9e3b5ff108": {  # temp values
         1337: "QmcBcc4nxdGiKyM6RC7fZAUtsYNaAYqfzf9wfNxYmdT9F2",  # dev
         1: "Qma9Fv7qSahePTd5ACCFG8Jsd3qJqtFUqGmmGKoVqriaj2",  # ethereum
-        5: "QmTmj4cizhWpEFCCqk5dP67yws7R2PPgCtb2bd2RgVPCb2",  # goerli
         10: "QmcBcc4nxdGiKyM6RC7fZAUtsYNaAYqfzf9wfNxYmdT9F2",  # optimism
         42161: "QmXH8oBNx1EjxYkSWpk8DMcyJgMxCk8Dujz1PswQGdGFy2",  # arbitrum
-        80001: "QmcFZKUX9qoo3StwVycJTAhwdiqbEjcwNvQVK246p3z1r2",  # mumbai
         11155111: "QmdDwVFoEEcgv5qnaTB8ncnXGMnqrhnA5nYpRr4ouWe4A2",  # sepolia
         421614: "QmVwhF77aVNzRUkMJNLDkeF9BtQMHLnfDY5ePpZ81uKLz2",  # arbitrum sepolia
     }
