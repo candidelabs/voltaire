dbca8c9bb7ac039cbe7b5da2620f184cbcfa217c
Sherif Abdelmoatty <sherif.ahmed990@gmail.com>
2025-07-01 16:16:05 +0300

    add EntryPointSimulationsMod for Arbitrum

---
+++ voltaire_bundler/contracts/EntryPointSimulationsV7Arb.sol
@@ -0,0 +1,278 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.23;
+
+/* solhint-disable avoid-low-level-calls */
+/* solhint-disable no-inline-assembly */
+
+import "https://github.com/eth-infinitism/account-abstraction/blob/releases/v0.7/contracts/core/EntryPoint.sol";
+
+interface IEntryPointSimulations is IEntryPoint {
+    // Return value of simulateHandleOp.
+    struct ExecutionResult {
+        uint256 preOpGas;
+        uint256 paid;
+        uint256 accountValidationData;
+        uint256 paymasterValidationData;
+        bool targetSuccess;
+        bytes targetResult;
+    }
+
+    /**
+     * Successful result from simulateValidation.
+     * If the account returns a signature aggregator the "aggregatorInfo" struct is filled in as well.
+     * @param returnInfo     Gas and time-range returned values
+     * @param senderInfo     Stake information about the sender
+     * @param factoryInfo    Stake information about the factory (if any)
+     * @param paymasterInfo  Stake information about the paymaster (if any)
+     * @param aggregatorInfo Signature aggregation info (if the account requires signature aggregator)
+     *                       Bundler MUST use it to verify the signature, or reject the UserOperation.
+     */
+    struct ValidationResult {
+        ReturnInfo returnInfo;
+        StakeInfo senderInfo;
+        StakeInfo factoryInfo;
+        StakeInfo paymasterInfo;
+        AggregatorStakeInfo aggregatorInfo;
+    }
+
+    /**
+     * Simulate a call to account.validateUserOp and paymaster.validatePaymasterUserOp.
+     * @dev The node must also verify it doesn't use banned opcodes, and that it doesn't reference storage
+     *      outside the account's data.
+     * @param userOp - The user operation to validate.
+     * @return the validation result structure
+     */
+    function simulateValidation(
+        PackedUserOperation calldata userOp,
+        uint256 minBlock
+    )
+    external
+    returns (
+        ValidationResult memory, uint256 currentBlockNumber, uint256 currentBlockTimeStamp, bytes32 currentBlockHash
+    );
+
+    /**
+     * Simulate full execution of a UserOperation (including both validation and target execution)
+     * It performs full validation of the UserOperation, but ignores signature error.
+     * An optional target address is called after the userop succeeds,
+     * and its value is returned (before the entire call is reverted).
+     * Note that in order to collect the the success/failure of the target call, it must be executed
+     * with trace enabled to track the emitted events.
+     * @param op The UserOperation to simulate.
+     * @param target         - If nonzero, a target address to call after userop simulation. If called,
+     *                         the targetSuccess and targetResult are set to the return from that call.
+     * @param targetCallData - CallData to pass to target address.
+     * @return the execution result structure
+     */
+    function simulateHandleOp(
+        PackedUserOperation calldata op,
+        address target,
+        bytes calldata targetCallData
+    )
+    external
+    returns (
+        ExecutionResult memory
+    );
+}
+
+interface ArbSys {
+     /**
+     * @notice Get Arbitrum block number (distinct from L1 block number; Arbitrum genesis block has block number 0)
+     * @return block number as int
+     */
+    function arbBlockNumber() external view returns (uint256);
+
+    /**
+     * @notice Get Arbitrum block hash (reverts unless currentBlockNum-256 <= arbBlockNum < currentBlockNum)
+     * @return block hash
+     */
+    function arbBlockHash(
+        uint256 arbBlockNum
+    ) external view returns (bytes32);
+}
+
+/*
+ * This contract inherits the EntryPoint and extends it with the view-only methods that are executed by
+ * the bundler in order to check UserOperation validity and estimate its gas consumption.
+ * This contract should never be deployed on-chain and is only used as a parameter for the "eth_call" request.
+ */
+contract EntryPointSimulationsModV7Arb is EntryPoint, IEntryPointSimulations {
+    // solhint-disable-next-line var-name-mixedcase
+    AggregatorStakeInfo private NOT_AGGREGATED = AggregatorStakeInfo(address(0), StakeInfo(0, 0));
+
+    SenderCreator private _senderCreator;
+
+    function initSenderCreator() internal virtual {
+        //this is the address of the first contract created with CREATE by this address.
+        //address createdObj = address(uint160(uint256(keccak256(abi.encodePacked(hex"d694", address(this), hex"01")))));
+        _senderCreator = SenderCreator(0xEFC2c1444eBCC4Db75e7613d20C6a62fF67A167C);
+    }
+
+    function senderCreator() internal view virtual override returns (SenderCreator) {
+        // return the same senderCreator as real EntryPoint.
+        // this call is slightly (100) more expensive than EntryPoint's access to immutable member
+        return _senderCreator;
+    }
+
+    /**
+     * simulation contract should not be deployed, and specifically, accounts should not trust
+     * it as entrypoint, since the simulation functions don't check the signatures
+     */
+    constructor() {
+        //require(block.number < 100, "should not be deployed");
+    }
+
+    function simulateValidation(
+        PackedUserOperation calldata userOp,
+        uint256 minBlock
+    )
+    external
+    returns (
+        ValidationResult memory, uint256 currentBlockNumber, uint256 currentBlockTimeStamp, bytes32 currentBlockHash
+    ){
+        ArbSys arbSys = ArbSys(0x0000000000000000000000000000000000000064);
+        uint256 arbBlockNumber = arbSys.arbBlockNumber();
+        require(minBlock == 0 || arbBlockNumber >= minBlock, "current block number is not higher than minBlock");
+
+        UserOpInfo memory outOpInfo;
+
+        _simulationOnlyValidations(userOp);
+        (
+            uint256 validationData,
+            uint256 paymasterValidationData
+        ) = _validatePrepayment(0, userOp, outOpInfo);
+        StakeInfo memory paymasterInfo = _getStakeInfo(
+            outOpInfo.mUserOp.paymaster
+        );
+        StakeInfo memory senderInfo = _getStakeInfo(outOpInfo.mUserOp.sender);
+        StakeInfo memory factoryInfo;
+        {
+            bytes calldata initCode = userOp.initCode;
+            address factory = initCode.length >= 20
+                ? address(bytes20(initCode[0 : 20]))
+                : address(0);
+            factoryInfo = _getStakeInfo(factory);
+        }
+
+        address aggregator = address(uint160(validationData));
+        ReturnInfo memory returnInfo = ReturnInfo(
+            outOpInfo.preOpGas,
+            outOpInfo.prefund,
+            validationData,
+            paymasterValidationData,
+            getMemoryBytesFromOffset(outOpInfo.contextOffset)
+        );
+
+        AggregatorStakeInfo memory aggregatorInfo = NOT_AGGREGATED;
+        if (uint160(aggregator) != SIG_VALIDATION_SUCCESS && uint160(aggregator) != SIG_VALIDATION_FAILED) {
+            aggregatorInfo = AggregatorStakeInfo(
+                aggregator,
+                _getStakeInfo(aggregator)
+            );
+        }
+        return (ValidationResult(
+            returnInfo,
+            senderInfo,
+            factoryInfo,
+            paymasterInfo,
+            aggregatorInfo
+        ), arbBlockNumber, block.timestamp, bytes32(0));
+    }
+
+    /// @inheritdoc IEntryPointSimulations
+    function simulateHandleOp(
+        PackedUserOperation calldata op,
+        address target,
+        bytes calldata targetCallData
+    )
+    external nonReentrant
+    returns (
+        ExecutionResult memory
+    ){
+        UserOpInfo memory opInfo;
+        _simulationOnlyValidations(op);
+        (
+            uint256 validationData,
+            uint256 paymasterValidationData
+        ) = _validatePrepayment(0, op, opInfo);
+
+        uint256 paid = _executeUserOp(0, op, opInfo);
+        bool targetSuccess;
+        bytes memory targetResult;
+        if (target != address(0)) {
+            (targetSuccess, targetResult) = target.call(targetCallData);
+        }
+        return ExecutionResult(
+            opInfo.preOpGas,
+            paid,
+            validationData,
+            paymasterValidationData,
+            targetSuccess,
+            targetResult
+        );
+    }
+
+    function _simulationOnlyValidations(
+        PackedUserOperation calldata userOp
+    )
+    internal
+    {
+        //initialize senderCreator(). we can't rely on constructor
+        initSenderCreator();
+
+        try
+        this._validateSenderAndPaymaster(
+            userOp.initCode,
+            userOp.sender,
+            userOp.paymasterAndData
+        )
+        // solhint-disable-next-line no-empty-blocks
+        {} catch Error(string memory revertReason) {
+            if (bytes(revertReason).length != 0) {
+                revert FailedOp(0, revertReason);
+            }
+        }
+    }
+
+    /**
+     * Called only during simulation.
+     * This function always reverts to prevent warm/cold storage differentiation in simulation vs execution.
+     * @param initCode         - The smart account constructor code.
+     * @param sender           - The sender address.
+     * @param paymasterAndData - The paymaster address (followed by other params, ignored by this method)
+     */
+    function _validateSenderAndPaymaster(
+        bytes calldata initCode,
+        address sender,
+        bytes calldata paymasterAndData
+    ) external view {
+        if (initCode.length == 0 && sender.code.length == 0) {
+            // it would revert anyway. but give a meaningful message
+            revert("AA20 account not deployed");
+        }
+        if (paymasterAndData.length >= 20) {
+            address paymaster = address(bytes20(paymasterAndData[0 : 20]));
+            if (paymaster.code.length == 0) {
+                // It would revert anyway. but give a meaningful message.
+                revert("AA30 paymaster not deployed");
+            }
+        }
+        // always revert
+        revert("");
+    }
+
+    //make sure depositTo cost is more than normal EntryPoint's cost,
+    // to mitigate DoS vector on the bundler
+    // empiric test showed that without this wrapper, simulation depositTo costs less..
+    function depositTo(address account) public override(IStakeManager, StakeManager) payable {
+        unchecked{
+        // silly code, to waste some gas to make sure depositTo is always little more
+        // expensive than on-chain call
+            uint256 x = 1;
+            while (x < 5) {
+                x++;
+            }
+            StakeManager.depositTo(account);
+        }
+    }
+}
