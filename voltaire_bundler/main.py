04740f1abc90e68196c314c3af7f23e5ac9e11d7
Andrew Wahid <andrew.wahid1@gmail.com>
2026-04-07 01:07:46 +0200

    feat: add --enforce_pre_verification_gas_tolerance CLI argument

--- voltaire_bundler/main.py
+++ voltaire_bundler/main.py
@@ -73,6 +73,7 @@ async def main(cmd_args=sys.argv[1:], loop=None) -> None:
                 init_data.max_fee_per_gas_percentage_multiplier,
                 init_data.max_priority_fee_per_gas_percentage_multiplier,
                 init_data.enforce_gas_price_tolerance,
+                init_data.enforce_pre_verification_gas_tolerance,
                 init_data.ethereum_node_debug_trace_call_urls,
                 init_data.ethereum_node_eth_get_logs_urls,
                 init_data.disable_p2p,
