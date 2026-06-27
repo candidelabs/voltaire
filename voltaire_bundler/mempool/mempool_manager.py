5e232e1d39e7b6610672424727c2077a7dbdfa64
Sherif Abdelmoatty <sherif.ahmed990@gmail.com>
2026-06-27 22:41:44 +0300

    fix typo

--- voltaire_bundler/mempool/mempool_manager.py
+++ voltaire_bundler/mempool/mempool_manager.py
@@ -397,7 +397,7 @@ class LocalMempoolManager():
                         "user operation skipped for bundling because "
                         "because max bundle gas limit was reached. "
                         f"user operation max gas: {user_operation_max_gas}, "
-                        f"compined cas limit: {compined_gas_limit + user_operation_max_gas}, "
+                        f"compined gas limit: {compined_gas_limit + user_operation_max_gas}, "
                         f"max compined bundle gas limit: {max_compined_bundle_user_operations_gas_limit}"
                     )
                     continue
